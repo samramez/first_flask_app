4cc69b6cb725aba3f09f1c3a5caee79394cf3439
Sam Ramezanli <samramezz@gmail.com>
2015-04-13 15:27:28 -0400

    Lesson 3 --> Start of using Templates

--- project.py
+++ project.py
@@ -18,9 +18,9 @@ session = DBSession()
 
 # If a path from the browser with /hello get executed, this function would  run
 @app.route('/')
-@app.route('/hello')
-def hello_world():
-    restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     output = ''
     for i in items:
@@ -32,6 +32,23 @@ def hello_world():
         output += "</br></br>"
 
     return output
+
+
+# Task 1: Create route for newMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+# Task 2: Create route for editMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+# Task 3: Create a route for deleteMenuItem function here
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
+
 
 
 if __name__ == '__main__':
