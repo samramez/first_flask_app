d6d7a631dcc7a401e5c238edc0720f66e3f57e83
Sam Ramezanli <samramezz@gmail.com>
2015-04-13 15:01:29 -0400

    Lesson 3 --> Start of Routing

--- project.py
+++ project.py
@@ -3,11 +3,35 @@ from flask import Flask
 # Making instance of the class
 app = Flask(__name__)
 
+# Import from database_setup (Lesson One)
+from database_setup import Base, Restaurant, MenuItem
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Create session and connect to DB ##
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+
+
 # If a path from the browser with /hello get executed, this function would  run
 @app.route('/')
 @app.route('/hello')
 def hello_world():
-    return 'Hello World!'
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += "</br>"
+        output += i.price
+        output += "</br>"
+        output += i.description
+        output += "</br></br>"
+
+    return output
 
 
 if __name__ == '__main__':
