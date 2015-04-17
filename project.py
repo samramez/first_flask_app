8f642621b343df0a6f54445f9e136d4e5457ce04
Sam Ramezanli <samramezz@gmail.com>
2015-04-17 00:51:22 -0400

    Lesson 3 --> End of DELETE

--- project.py
+++ project.py
@@ -72,9 +72,17 @@ def editMenuItem(restaurant_id, MenuID):
         return render_template('editmenuitem.html', restaurant_id = restaurant_id, MenuID = MenuID, item = editedItem)
 
 # Task 3: Create a route for deleteMenuItem function here
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/' , methods= ['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menu_id):
-    return "page to delete a menu item. Task 3 complete!"
+
+    deletedItem = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
+        session.delete(deletedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+
+    else:
+        return render_template('deletemenuitem.html', restaurant_id = restaurant_id, MenuID = menu_id, item=deletedItem)
 
 
 
